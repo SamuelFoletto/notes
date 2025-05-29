23d1fc168bbbe1bf49b0d831ebd1f9717213a6e7
samue <sfoletto@gmail.com>
2025-05-28 22:24:33 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/about', function(){
     echo 'About Us';
 });
+
+
+Route::get('/main/{value}', [MainController::class, 'index']);
+    
